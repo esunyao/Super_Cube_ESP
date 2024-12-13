591d5be7bfe2ffc367c18442df50924a03c403e1
Esuny <esun.yao@hotmail.com>
2024-12-14 01:59:34 +0800

    修改姿态传感器模块

--- Super_Cube_ESP/include/service/AttitudeService.h
+++ Super_Cube_ESP/include/service/AttitudeService.h
@@ -5,16 +5,26 @@
 #ifndef SUPER_CUBE_ESP_ATTITUDESERVICE_H
 #define SUPER_CUBE_ESP_ATTITUDESERVICE_H
 #include <super_cube.h>
+#include "MPU6050_6Axis_MotionApps20.h"
 
 class AttitudeService {
 public:
     AttitudeService(super_cube *cube);
+    ~AttitudeService();
     void setup();
     void update();
     void getMotion6(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz);
-
+    void OffsetSet(int16_t XG, int16_t YG, int16_t ZG, int16_t XA, int16_t YA, int16_t ZA);
+    static AttitudeService *instance; // 静态实例指针
 private:
     super_cube *superCube;
+    MPU6050 *mpu;
+    /*---MPU6050控制/状态变量---*/
+    bool DMPReady = false;  // 如果DMP初始化成功，则设置为true
+    uint8_t devStatus;      // 每次设备操作后的返回状态（0 = 成功，!0 = 错误）
+    uint16_t packetSize;    // 预期的DMP数据包大小（默认是42字节）
+    uint8_t FIFOBuffer[64]; // FIFO存储缓冲区
+    Quaternion q;           // [w, x, y, z]         四元数容器
 };
 
 #endif //SUPER_CUBE_ESP_ATTITUDESERVICE_H
